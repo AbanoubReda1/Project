1b35efc2b2d16c516de8d7c029d611597707457c
AbanoubReda1 <Abanoubreda68@gmail.com>
2019-11-12 17:54:51 +0200

    Add Some futures

---
+++ StudentService/StudentService/Controllers/TasksController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentService.Models;
+using Task = StudentService.Models.Task;
+
+namespace StudentService.Controllers
+{
+    public class TasksController : Controller
+    {
+        private StudentServiceEntities db = new StudentServiceEntities();
+
+        // GET: Tasks
+        public async Task<ActionResult> Index()
+        {
+            var tasks = db.Tasks.Include(t => t.Type);
+            return View(await tasks.ToListAsync());
+        }
+
+        // GET: Tasks/Details/5
+        public async Task<ActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.Task task = await db.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // GET: Tasks/Create
+        public ActionResult Create()
+        {
+            ViewBag.FKTypeCode = new SelectList(db.Types, "TypeCode", "TypeDescription");
+            return View();
+        }
+
+        // POST: Tasks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "TaskNo,TaskPath,FKTypeCode")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FKTypeCode = new SelectList(db.Types, "TypeCode", "TypeDescription", task.FKTypeCode);
+            return View(task);
+        }
+
+        // GET: Tasks/Edit/5
+        public async Task<ActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Models.Task task = await db.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FKTypeCode = new SelectList(db.Types, "TypeCode", "TypeDescription", task.FKTypeCode);
+            return View(task);
+        }
+
+        // POST: Tasks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "TaskNo,TaskPath,FKTypeCode")] Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(task).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FKTypeCode = new SelectList(db.Types, "TypeCode", "TypeDescription", task.FKTypeCode);
+            return View(task);
+        }
+
+        // GET: Tasks/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Task task = await db.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Tasks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(long id)
+        {
+            Task task = await db.Tasks.FindAsync(id);
+            db.Tasks.Remove(task);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
